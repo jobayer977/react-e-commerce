650ff7cc78db6e959bee242bcbec146d4a97fc86
Jobayer Hossain <jobayerhossain977@Gmail.com>
2020-10-04 09:40:25 +0600

    implement redux-persist and configuration cart reducer

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,18 @@
 import { combineReducers } from "redux";
+//REDUX PERSIS CONFIG
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+//ALL REDUCER LISTS
 import cartReducer from "./cart/cart.reducers";
 import userReducer from "./user/user.reducer";
 
-export default combineReducers({
-	user: userReducer,
-	cart: cartReducer,
-});
+const persistConfig = {
+	key: "root",
+	storage,
+	whiteList: ["cart"],
+};
+
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+
+export default persistReducer(persistConfig, rootReducer);
