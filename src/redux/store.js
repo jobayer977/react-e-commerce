81a8798e2ab1cda30577b83eadd8b05fe5832454
Jobayer Hossain <jobayerhossain977@Gmail.com>
2020-10-04 20:03:32 +0600

    removeing redux logger for porduction build

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,9 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 const middleware = [logger];
+if (process.env.NODE_ENV === "development") {
+	middleware.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware));
 
